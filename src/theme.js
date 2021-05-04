66aef2ec8a56f1ce53d34cb9550f89f165af0d7f
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-05-04 11:37:30 +0530

    Added Theme ans Sytling to MUI

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import baseStyle from './styles/base.scss'
+
+console.log(baseStyle.primary)
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1C1C1E',
+    },
+  }
+})
