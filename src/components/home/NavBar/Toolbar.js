0e22bd7fe58046331e07fb667c67712ece323023
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-05-10 23:32:22 +0530

    #12: Added links for buttons

--- src/components/home/NavBar/Toolbar.js
+++ src/components/home/NavBar/Toolbar.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,13 +10,17 @@ function Toolbar({ menuId, onProfileMenuOpen, mobileMenuId, onMobileMenuOpen }) 
   return (
     <>
       <div className='home-navbar--section-desktop'>
-        <Button classes={{ root: 'home-navbar--button' }}>
-          <Typography variant="button">Sign Up</Typography>
-        </Button>
+        <Link to="/signup">
+          <Button classes={{ root: 'home-navbar--button' }}>
+            <Typography variant="button">Sign Up</Typography>
+          </Button>
+        </Link>
 
-        <Button classes={{ root: 'home-navbar--button' }} variant="outlined">
-          <Typography variant="button">Sign In</Typography>
-        </Button>
+        <Link to="/signin">
+          <Button classes={{ root: 'home-navbar--button' }} variant="outlined">
+            <Typography variant="button">Sign In</Typography>
+          </Button>
+        </Link>
       </div>
 
       <div className='home-navbar--section-mobile'>
