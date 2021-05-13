d4ac89d7dc4ead19adbec821fb1a4a4d5e56c963
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-05-13 18:33:05 +0530

    Fixed issues

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import NavBar from '../components/dashboard/NavBar';
 import { useLocation } from "react-router-dom";
+import NavBar from '../components/dashboard/NavBar';
 import ProjectsGridViewHeader from '../components/dashboard/ProjectsGridViewHeader';
 import ProjectsGridView from '../components/dashboard/ProjectsGridView';
 
@@ -9,19 +9,24 @@ import '../styles/Dashboard.scss'
 import fixtures from '../static/fixtures/dashboard.json'
 
 function Dashboard() {
-  const [searchFilter, setSearchFilter] = useState("");
   let location = useLocation();
   const queryParams = Object.fromEntries(location.search.slice(1).split('&').map((item) => item.split('=')));
+  const [searchFilter, setSearchFilter] = useState("");
+  const [isLoading, setLoading] = useState(!queryParams['fixtures']);
+  const [userProjects, setUserProjects] = useState((queryParams['fixtures']) ? fixtures : []);
   
-  // TODO: Connect backend
-  var [userProjects, setUserProjects] = useState({});
-  
-  useEffect(async () => {
-    const response = await axios.get('http://localhost:8000/api/users/1/projects/')
-        setUserProjects(response.data);
-        console.log(userProjects);
-  });
-  // const userProjects = (queryParams['fixtures']) ? fixtures : [];
+  useEffect(() => {
+    async function loadData() {
+      if(queryParams['fixtures']) return;
+
+      // # TODO: 1 should be replaced with user id with Redux
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/users/1/projects/`)
+      setUserProjects(response.data);
+      setLoading(false);
+      console.log(response)
+    }
+    loadData();
+  }, []);
 
   return (
     <div class="dashboard-root">
@@ -29,7 +34,8 @@ function Dashboard() {
       <div className="dashboard-projects-root">
         <ProjectsGridViewHeader />
         <ProjectsGridView 
-          userProjects={userProjects.filter((item) => (searchFilter == "" || item.name.toLowerCase().startsWith(searchFilter)))} 
+          userProjects={userProjects.filter((item) => (searchFilter === "" || item.name.toLowerCase().startsWith(searchFilter)))} 
+          isLoading={isLoading}
         />
       </div>
       {/* <div className="dashboard-footer">#Todo: Footer</div> */}
