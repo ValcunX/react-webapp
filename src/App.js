654306d54d5130182289733339ddfd085392dc4d
nikhiltadikonda <nikhiltadikonda@gmail.com>
2021-05-13 19:17:57 +0530

    #31 Added Redux to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import './styles/App.scss';
+import "./styles/App.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import Home from './pages/Home';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import NotFound from './pages/NotFound';
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
-      </Router>
+      <Provider store={{"user":"demo", "email": "123@123"}}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="*" component={NotFound} />
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
